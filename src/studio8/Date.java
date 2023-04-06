1f28d11afc6716746f60ae6ce4e7d14d9be64bc7
grantkaplan <grantkaplan@localhost>
2023-04-06 15:26:07 -0500

    done :)

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,79 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
 
+private int month;
+private int day;
+private int year;
+private boolean holiday;
+/**
+ * 
+ * @param month
+ * @param day
+ * @param year
+ * @param holiday
+ */
+    public Date(int month, int day, int year, boolean holiday) {
+	this.month = month;
+	this.day = day;
+	this.year = year;
+	this.holiday = holiday;
+}
 
-    public static void main(String[] args) {
-
+    public String toString() {
+    	if (holiday) {
+    		return month + "/" + day + "/" + year + " It's a holiday!";
+    	}
+    	return month + "/" + day + "/" + year;
     }
+    
+	public static void main(String[] args) {
+		Date date1 = new Date(7,4,2008,true);
+		Date date2 = new Date(7,4,2002,false);
+		Date date3 = new Date(9,11,2006, true);
+		Date date4 = new Date(6,29,2004, false);
+		Date date5 = new Date(12, 30, 2003, false);
+		
+		LinkedList<Date> list = new LinkedList<Date>();
+		list.add(date1);
+		list.add(date2);
+		list.add(date3);
+		list.add(date4);
+		list.add(date5);
+		
+		System.out.println(list);
+		
+		HashSet<Date> set = new HashSet<Date>();
+		set.add(date1);
+		set.add(date2);
+		set.add(date3);
+		set.add(date4);
+		set.add(date5);
+		
+		System.out.println(set);
+    }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month;
+	}
+
+	
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,69 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
 
+	private int hour;
+	private int minute;
+	private boolean military;
+	
+/**
+ * 
+ * @param hour
+ * @param minute
+ * @param military
+ */
+	
+	public Time(int hour, int minute, boolean military) {
+		this.hour = hour;
+		this.minute = minute;
+		this.military = military;
+	}
+
+
+	public String toString() {
+		if (military) {
+		 return hour + ":" + minute;
+		}
+		if (hour>12) {
+			 return (hour -12) + ":" + minute;
+		}
+		else {
+			 return hour + ":" + minute;
+		}
+	
+	 }
+	 
 	public static void main(String[] args) {
- 
-    	
+		Time time = new Time(2,51,true);
+		Time newTime = new Time(2,51,true);
+				System.out.println(time);
+				System.out.println(newTime);
+				
+			if(time.equals(newTime)) {
+				System.out.println(true); 
+			}
     }
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, military, minute);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && military == other.military && minute == other.minute;
+	}
+	
 
 }
